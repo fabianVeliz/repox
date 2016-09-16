723ea5e08da72116d46ea5247f473b9843dc2d5b
Fabián Veliz <velizfabianhoracio@gmail.com>
2016-09-15 23:51:26 -0300

    Bootstrap application

---
+++ app.js
@@ -0,0 +1,10 @@
+class App extends React.Component {
+  render() {
+    return <div>
+      Repox
+    </div>;
+  }
+} 
+
+// Render app
+ReactDOM.render(<App />, document.getElementById('content'));
